1cc36ed73e36951c3ae40a0a7468a76591d18a3e
Hector Novoa <hectornovoadita2000@gmail.com>
2022-08-10 01:37:24 -0500

    Android studio app, custom interface.

---
+++ SMSGPS/app/src/main/java/com/example/sms_gps/MainActivity.java
@@ -0,0 +1,18 @@
+ package com.example.sms_gps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.animation.TimeAnimator;
+import android.os.Bundle;
+import android.widget.Toast;
+
+ public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) { //Método OnCreate
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+ }
