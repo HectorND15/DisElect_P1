4f2de5fbf770cea0151ba45e33c103c8cf1d4a1d
Hector <hectornovoadita2000@gmail.com>
2022-08-11 08:14:42 -0500

    This app receive and send via SMS the Latitude and Longitude parameters.

--- SMSGPS/app/src/main/java/com/example/sms_gps/MainActivity.java
+++ SMSGPS/app/src/main/java/com/example/sms_gps/MainActivity.java
@@ -1,112 +1,116 @@
- package com.example.sms_gps;
+package com.example.sms_gps;
 
-import androidx.annotation.NonNull;
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+import static android.Manifest.permission.READ_PHONE_NUMBERS;
+import static android.Manifest.permission.READ_PHONE_STATE;
+import static android.Manifest.permission.READ_SMS;
+import static android.Manifest.permission.SEND_SMS;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.telephony.SmsManager;
+import android.telephony.TelephonyManager;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.os.Build;
 
- public class MainActivity extends AppCompatActivity {
+import java.text.DecimalFormat;
 
-     Button send;
-     TextView tvLatitude;
-     TextView tvLongitude;
-     EditText phoneNumber;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvLatitude;
+    TextView tvLongitude;
+    EditText phoneNumber;
+
+    private LocationManager ubication;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) { //Método OnCreate
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        send = (Button) findViewById(R.id.send); //Instanciar Boton
-        tvLatitude = (TextView) findViewById(R.id.tvLatitude); //Instancio el TextView de Latitud
-        tvLongitude = (TextView) findViewById(R.id.tvLongitude);
-        phoneNumber = (EditText) findViewById(R.id.phoneNumber);
-
-        send.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                if(checkSelfPermission(Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_DENIED){
-                    requestPermissions(new String[]{Manifest.permission.SEND_SMS}, 1);
-                }
-
-                //Acquiere a reference to the system Location Manager
-                LocationManager locationManager = (LocationManager) MainActivity.this.getSystemService(Context.LOCATION_SERVICE);
-
-                //Define a Listener that reponds to location updates
-                LocationListener locationListener = new LocationListener() {
-                    @Override
-                    public void onLocationChanged(@NonNull Location location) {
-                        tvLatitude.setText(""+location.getLatitude());
-                        tvLongitude.setText(""+location.getLongitude());
-                        sendSMS(location.getLatitude(),location.getLongitude());
-                    }
-
-                    public void onStatusChanged(String provider,int status, Bundle extra){                    }
-                    public void onProviderEnable(String provider){                    }
-                    public void onProviderDisable(String provider){                    }
-
-
-                };
-                int permissionCheck = ContextCompat.checkSelfPermission(MainActivity.this,
-                        Manifest.permission.ACCESS_FINE_LOCATION);
-                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
-
-            }
-
-
-        });
-
-
-
-
-        /*-----------PERMISSION BLOCK---------------*/
-        int permissionCheck = ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION); //Verificar si tenemos permiso de la ubicación
-
-        if (permissionCheck== PackageManager.PERMISSION_DENIED){ //Si no tiene o está negado, preguntar por el permiso
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.ACCESS_FINE_LOCATION))
-            {
-
-            }else {
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
-            }
-        }
-
-        /*-------------------------------------------*/
     }
-    private void sendSMS(double Latitude, double Longitude){
-        String phNumber = phoneNumber.getText().toString().trim();
-        if (phNumber == ""){
-            Toast.makeText(this, "Please, Input a phone number", Toast.LENGTH_SHORT).show();
-        } else{
+
+    public void pushed(View v) {
+        tvLatitude = (TextView) findViewById(R.id.tvLatitude);
+        tvLongitude = (TextView) findViewById(R.id.tvLongitude);
+        if (localization() != null){
+            double[] ubication = localization();
+            tvLatitude.setText("" + ubication[0]);
+            tvLongitude.setText("" + ubication[1]);
+            sendSMS(ubication[0], ubication[1]);
         }
-            String SMS =  "Ubicación:\nLatitud:   "+Latitude+"\nLongitud:   "+Longitude;
+    }
+
+    @Nullable
+    private double[] localization() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            ubication = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            Location loc = ubication.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            double[] ubication = new double[2];
+            ubication[0] = loc.getLatitude();
+            ubication[1] = loc.getLongitude();
+            return ubication;
+        }else {
+            // Ask for permission
+            requestPermission();
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+
+        }
+        return null;
+    }
+
+    private void sendSMS(double Latitude, double Longitude) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED) {
+
+            phoneNumber = (EditText) findViewById(R.id.phoneNumber);
+            String phNumber = phoneNumber.getText().toString().trim();
+            String SMS =
+                    "UBICACION\n" +
+                            "Latitud:     " + Latitude + "\n" +
+                            "Longitud:    " + Longitude;
+
             try {
                 SmsManager smsManager = SmsManager.getDefault();
-                smsManager.sendTextMessage(phNumber,null,SMS,null,null);
+                smsManager.sendTextMessage(phNumber, null, SMS, null, null);
                 Toast.makeText(this, "Message Sent", Toast.LENGTH_SHORT).show();
-            }catch (Exception e){
+            } catch (Exception e) {
                 e.printStackTrace();
-                Toast.makeText(this, "Failed", Toast.LENGTH_SHORT).show();
-                }
+                Toast.makeText(this, "Failed to send", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            // Ask for permission
+            requestPermission();
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.SEND_SMS}, 1);
         }
+
     }
+
+    private void requestPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(new String[]{READ_SMS, READ_PHONE_NUMBERS, READ_PHONE_STATE, SEND_SMS, ACCESS_COARSE_LOCATION, ACCESS_FINE_LOCATION}, 100);
+            }
+        }
+
+
+    }
+
+
+
+
 
 
 
