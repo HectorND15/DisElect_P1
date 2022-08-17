c1897e7f232554dd2a290f63f9500de58533f111
Hector <hectornovoadita2000@gmail.com>
2022-08-17 02:40:32 -0500

    Semana 2 del proyecto, solo ajusté la personalización de la app

---
+++ SMSGPS/app/src/main/java/com/example/sms_gps/LocationIP.java
@@ -0,0 +1,14 @@
+package com.example.sms_gps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LocationIP extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location_ip);
+    }
+}
